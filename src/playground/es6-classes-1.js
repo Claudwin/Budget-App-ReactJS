d458c66d1034a28783e4c24e89aaf06d146f710b
Claudwin <claudwin.fortune@gmail.com>
2019-02-23 13:55:40 -0700

    es6 classes advanced

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -13,8 +13,42 @@ class Person {
     }
   } 
 
-const me = new Person('Claude Fortune', 23);
-console.log(me.getDescription());
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}`
+    }
+    return description
+  }
+}
 
-const other = new Person();
-console.log(other.getDescription());
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+  hasHomeLocation() {
+    return !!this.homeLocation;
+  }
+  getGretting() {
+    let greeting = super.getGretting();
+    if (this.hasHomeLocation()) {
+      greeting += ` I'm visiting from ${this.homeLocation}`
+    }
+    return greeting;
+  }
+}
+const me = new Traveler('Claude Fortune', 23, 'Edmonton');
+console.log(me.getGretting());
+
+const other = new Traveler();
+console.log(other.getGretting());
+
