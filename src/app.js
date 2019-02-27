e2ded38286ed3f5cc56e86a19dd3dc2dcf73d5bf
Claudwin <claudwin.fortune@gmail.com>
2019-02-27 09:57:15 -0700

    Setup babel with webpack

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <p>Testing 123</p>
+
+ReactDOM.render(template, document.getElementById('app'));
