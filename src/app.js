0d1c07a87cbd6255bffd26d827397372a2748005
Claudwin <claudwin.fortune@gmail.com>
2019-02-27 08:18:51 -0700

    react & react DOM install npm modules

--- src/app.js
+++ src/app.js
@@ -1,7 +1,1 @@
-//import './playground/utils';
-import subtract, { square, add } from './playground/utils'
-
-console.log('app.js is running!')
-console.log(square(4));
-console.log(add(100, 23));
-console.log(subtract(100, 19));
+import React from 'react'
