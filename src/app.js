f7c8b6e5deabdcf9c095b18290ac619f7406d4cb
Claudwin <claudwin.fortune@gmail.com>
2019-03-01 11:04:59 -0700

    setting up NavLink

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -30,11 +30,24 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
   <div>
-    404!
+    404!- <Link to="/">Go Home</Link>
   </div>
 );
+
+const Header = () => (
+  <header>
+    <h1>Budget App</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Go Home</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create expense page</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit page</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help Page</NavLink>
+  </header>
+);
+
 const routes = (
   <BrowserRouter>
+    <div>
+      <Header />
     <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
@@ -42,6 +55,7 @@ const routes = (
       <Route path="/help" component={HelpPage} />
       <Route component={NotFoundPage} />
     </Switch>
+    </div>
   </BrowserRouter>
 );
 
