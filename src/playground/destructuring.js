ddaba0299e54ad4c751899d1538bc2012eca60bb
Claudwin <claudwin.fortune@gmail.com>
2019-03-01 19:37:56 -0700

    2s6 array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -12,3 +12,8 @@ console.log(`${firstName} is ${age}`);
 
 const {city, temp: temperature} = person.location;
 console.log(`It's ${temperature} in ${city}`)
+
+
+const address = ['123', 'NYC', 'NY', '11433']
+
+const []
