4d49297a914c6b139405dbe18d788dafcf0f15a1
Claudwin <claudwin.fortune@gmail.com>
2019-03-23 16:00:34 -0600

    fix error in server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '../', 'public');
-const port = process.env.Port || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
